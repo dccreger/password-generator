e1921a094a00157a818f4fdf36f920b8dda2fd15
Chance Creger <chance.creger@gmail.com>
2023-11-28 11:34:31 -0700

    added JS code to generate password with different types of characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 // Assignment code here
-
-
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChars = "1234567890";
+var specialChars = "!@#$%^&*()-=+[]{};:'<>/?";
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,8 +12,71 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var length = getPasswordLength();
+  var includeLowercase = getIncludeLowercase();
+  var includeUppercase = getIncludeUppercase();
+  var includeNumbers = getIncludeNumbers();
+  var includeSpecial = getIncludeSpecial();
+
+  var allChars = "";
+
+  if (includeLowercase) {
+    allChars += lowercaseChars;
+  }
+
+  if (includeUppercase) {
+    allChars += uppercaseChars;
+  }
+  if (includeNumbers) {
+    allChars += numberChars;
+  }
+  if (includeSpecial) {
+    allChars += numberChars;
+  }
+  if (allChars === "") {
+    alert("Please select at least one character type");
+    return generatePassword();
+  }
+
+  var generatedPassword = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * allChars.length);
+    generatedPassword += allChars.charAt(randomIndex);
+  }
+  return generatedPassword;
+}
+
+function getPasswordLength() {
+  var userLengthInput = prompt(
+    "How many characters would you like your password to be? Min: 8 Max: 128"
+  );
+  var passwordLength = parseInt(userLengthInput);
+
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter valid number between 8 and 128.");
+    return getPasswordLength();
+  }
+  return passwordLength;
+}
+
+function getIncludeLowercase() {
+  return confirm("Would you like to include lowercase characters?");
+}
+
+function getIncludeUppercase() {
+  return confirm("Would you like to include uppercase characters?");
+}
+
+function getIncludeNumbers() {
+  return confirm("Would you like to include numbers?");
+}
+
+function getIncludeSpecial() {
+  return confirm("Would you like to include special characters?");
+}
